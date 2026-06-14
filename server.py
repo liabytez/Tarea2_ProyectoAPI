88cec10f80a96622d96341e9e56a7d0bd79fe547
Lia <h.mountain.phillips@gmail.com>
2026-06-14 15:17:23 +0100

    backend terminado - fin de proyecto

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 # Configuración de la base de datos
 DB_CONFIG = {
